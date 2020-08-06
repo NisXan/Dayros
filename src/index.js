d816f8b15a98e165920aed64f5646b54d28544e3
NisXan <nisxan@mail.ru>
2020-08-06 11:37:56 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import { Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './containers/App.js';
 import reducers from './reducers/reducers.js';
 import { setAccessTokenUnplash, authenticationUrl, setBearerToken } from './unsplash/unsplash.js'; 
-import { createBrowserHistory } from 'history';
 
 import '../public/favicon.ico';
 
@@ -18,12 +17,10 @@ export const store = createStore(
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
-const history = createBrowserHistory();
 const code = location.search.split('code=')[1];
 
 if (code) {  
   setAccessTokenUnplash(code);
-  history.push('/');
 }
 
 if (localStorage.getItem('token') === 'undefined' || localStorage.getItem('token') === ''|| !localStorage.getItem('token')) {
@@ -32,7 +29,7 @@ if (localStorage.getItem('token') === 'undefined' || localStorage.getItem('token
   setBearerToken(localStorage.getItem('token'));
   ReactDOM.render(
     <Provider store = { store }>
-      <Router history= { history }>
+      <Router>
         <App />
       </Router>
     </Provider>,
